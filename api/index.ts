18703509949b814110216bf7f73a680217751f33
Wubpooz <mathieu.waharte@gmail.com>
2026-06-02 14:32:27 +0000

    trying to deploy backend on Vercel

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,3 @@
 import app from '../backend/src/index';
 
-// Export the fetch handler natively for Vercel's Node environment
-export const RequestHandler = app.fetch;
-export default RequestHandler;
+export default app;
