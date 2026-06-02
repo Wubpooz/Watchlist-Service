7dd337772683041b670b2de647f4fa3ae4ee545a
Wubpooz <mathieu.waharte@gmail.com>
2026-06-02 17:15:12 +0000

    fixes

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from "prisma/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "src/db/seed.ts",
+    seed: "bun run src/db/seed.ts",
   },
   datasource: {
-    url: process.env.POSTGRES_URL || "postgresql://johndoe:randompassword@localhost:5432/mydb",
-    directUrl: process.env.POSTGRES_URL_NON_POOLING
+    url: env("POSTGRES_URL") || "postgresql://johndoe:randompassword@localhost:5432/mydb"
   },
 });
