546b2131ff605a8e64c70ef3e793dbd4a3de82af
Wubpooz <mathieu.waharte@gmail.com>
2026-06-07 19:12:08 +0200

    removed unused logout functionality from App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,7 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
-import { useAuthStore } from '@/stores/auth';
-
-const router = useRouter();
-const authStore = useAuthStore();
-
-const handleLogout = () => {
-  authStore.logout();
-  router.push({ name: 'Login' });
-};
 </script>
 
 <template>
   <RouterView />
 </template>
 
-<style scoped>
-</style>
